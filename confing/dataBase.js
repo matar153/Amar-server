e304da096b56fa2ba2bf6e5fc99b2a324f183151
matar cohen <matarcohen153@gmail.com>
2023-07-23 21:23:40 +0300

    a

--- confing/dataBase.js
+++ confing/dataBase.js
@@ -1,5 +1,5 @@
 const monngse = require('mongoose');
 
 
-monngse.connect('mongodb://141.226.241.38:27017/amar')
-// monngse.connect('mongodb://127.0.0.1:27017/amar')
+// monngse.connect('mongodb://141.226.241.38:27017/amar')
+monngse.connect('mongodb://127.0.0.1:27017/amar')
